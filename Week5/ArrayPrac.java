f1b56d1f950845005d5c5507ccf1411fc7b32b85
Logan Oram <logan.oram@keyin.com>
2024-10-03 15:05:06 -0230

    Some code added

---
+++ Week5/ArrayPrac.java
@@ -0,0 +1,23 @@
+package Week5;
+
+// java method to calculate sum  and average of all elements of an integer array size (10)
+
+public class ArrayPrac {
+    
+    public static void main(String[] args) {
+
+        int size = 10;
+        int array1[] = new int[size];
+        int array2[] = {22, 13, 14, 20, 48, 91};
+        char chArray[] = new char[size];
+        char chArray2[] = {'H','E','L','L','O'};
+        String s1 = "Hello";
+        s1 = "Hello world, how are you";
+        String stringarray[] = new String[size];
+
+        for (int i = 0;i<array2.length;i++)
+        {
+            System.out.println(array2[i]);
+        }
+    }
+}
