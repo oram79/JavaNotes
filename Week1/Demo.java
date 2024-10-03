92c86269a0e273b01768350cf5331386e50eda2f
Logan Oram <logan.oram@keyin.com>
2024-10-03 14:40:28 -0230

    Comment out

--- Week1/Demo.java
+++ Week1/Demo.java
@@ -1,15 +1,15 @@
 
-public class Demo {
-    public static void main(String[] args) {
-        Student S1 = new Student();//default constructor is no more accessible as i have added my own
-        Student S2 = new Student();
+// public class Demo {
+//     public static void main(String[] args) {
+//         Student S1 = new Student();//default constructor is no more accessible as i have added my own
+//         Student S2 = new Student();
 
-    S1.display();
+//     S1.display();
    
 
-    S1.set("Hiba",20, "SD", 3);
+//     S1.set("Hiba",20, "SD", 3);
 
-    S1.display();
+//     S1.display();
         
-    }
-}
+//     }
+// }
